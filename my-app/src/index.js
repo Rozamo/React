06e3dbc75f61d4a781a91e0a92d82b5c123d1442
Rozamo <shivamanand.murmu.cse16@itbhu.ac.in>
2020-07-31 15:33:37 +0530

    Commenting

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 // import './index.css';
 // import App from './App';
 import Counter from './components/Counter';
-import ManyProps from './components/ManyProps';
-import ClickIncrement from './components/ClickIncrement';
-import FClickIncrement from './components/FClickIncrement';
-import Form from './components/Form';
-import ContactManager from './components/ContactManager';
-import ReduxIncrementManager from './components/ReduxIncrementManager';
-import ReduxContactManager from './components/ReduxContactManager';
+// import ManyProps from './components/ManyProps';
+// import ClickIncrement from './components/ClickIncrement';
+// import FClickIncrement from './components/FClickIncrement';
+// import Form from './components/Form';
+// import ContactManager from './components/ContactManager';
+// import ReduxIncrementManager from './components/ReduxIncrementManager';
+// import ReduxContactManager from './components/ReduxContactManager';
 import * as serviceWorker from './serviceWorker';
 
 var counter = 0;
@@ -18,13 +18,13 @@ function show() {
   ReactDOM.render(<Counter counter={counter}/>, document.getElementById('counter'));
 }
 setInterval(show, 1000);
-ReactDOM.render(<ManyProps/>, document.getElementById('manyprops'));
-ReactDOM.render(<ClickIncrement/>, document.getElementById('clickincrement'));
-ReactDOM.render(<FClickIncrement/>, document.getElementById('fclickincrement'));
-ReactDOM.render(<Form/>, document.getElementById('form'));
-ReactDOM.render(<ContactManager/>, document.getElementById('contactmanager'));
-ReactDOM.render(<ReduxIncrementManager/>, document.getElementById('reduxincrement'));
-ReactDOM.render(<ReduxContactManager/>, document.getElementById('reduxcontactmanager'));
+// ReactDOM.render(<ManyProps/>, document.getElementById('manyprops'));
+// ReactDOM.render(<ClickIncrement/>, document.getElementById('clickincrement'));
+// ReactDOM.render(<FClickIncrement/>, document.getElementById('fclickincrement'));
+// ReactDOM.render(<Form/>, document.getElementById('form'));
+// ReactDOM.render(<ContactManager/>, document.getElementById('contactmanager'));
+// ReactDOM.render(<ReduxIncrementManager/>, document.getElementById('reduxincrement'));
+// ReactDOM.render(<ReduxContactManager/>, document.getElementById('reduxcontactmanager'));
 
 
 // If you want your app to work offline and load faster, you can change
